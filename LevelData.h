ae14620a67690b3dfc68d0263a641abbda5bedf9
Towwwi <55107172+Towwwi@users.noreply.github.com>
2024-12-08 13:39:07 +0100

    Update LevelData.h

--- LevelData.h
+++ LevelData.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Copyright Tommi Kekomäki 2023. All Rights Reserved.
 
 #pragma once
 
@@ -7,52 +7,50 @@
 #include "GameplayTags.h"
 #include "LevelData.generated.h"
 
-
-
-USTRUCT()
+USTRUCT(Blueprintable)
 struct FLevelDataStruct
 {
 	GENERATED_BODY()
 
-public:
-	UPROPERTY(EditAnywhere, Category="Level info")
+	UPROPERTY(EditAnywhere, Category="Level info", BlueprintReadOnly)
 	TSoftObjectPtr<UWorld> Level;
 	
 	//Should we use seamless travel if possible to travel this level
-	UPROPERTY(EditAnywhere, Category="Traveling")
+	UPROPERTY(EditAnywhere, Category="Traveling", BlueprintReadOnly)
 	bool bUseSeamlessTravel = true;
 
 	// Do we want to notify the game after traveling has finished?
-	UPROPERTY(EditAnywhere, Category="Traveling")
+	UPROPERTY(EditAnywhere, Category="Traveling", BlueprintReadOnly)
 	bool bShouldSkipGameNotify = false;
 	
 	// Only used on ClientTravel
 	// Travel_Absolute -> Corresponds to a non-seamless travel (Hard reset)
 	// Travel_Partial  -> Corresponds to a non-seamless travel (Soft reset)
 	// Travel_Relative -> Corresponds to a seamless travel
-	UPROPERTY(EditAnywhere, Category="ClientTravel TravelType")
+	UPROPERTY(EditAnywhere, Category="ClientTravel TravelType", BlueprintReadOnly)
 	TEnumAsByte<ETravelType> TravelType = TRAVEL_Relative;
 
 	// Should we travel after a certain time?
-	UPROPERTY(EditAnywhere, Category="Traveling")
+	UPROPERTY(EditAnywhere, Category="Traveling", BlueprintReadOnly)
 	bool bUseTimerToTravel = false;
 
-	UPROPERTY(EditAnywhere, Category="Traveling", meta = (EditCondition = "bUseTimerToTravel"))
+	UPROPERTY(EditAnywhere, Category="Traveling", meta = (EditCondition = "bUseTimerToTravel"), BlueprintReadOnly)
 	bool bUseFade = false;
 	
 	// If UseTimerToTravel is true, this is the time amount to wait until travel happens
-	UPROPERTY(EditAnywhere, Category="Traveling", meta = (EditCondition = "bUseTimerToTravel"))
+	UPROPERTY(EditAnywhere, Category="Traveling", meta = (EditCondition = "bUseTimerToTravel"), BlueprintReadOnly)
 	float Timer;
 };
 
 /**
- * 
+ *  Data-asset to control how should we travel to the wanted level
  */
 UCLASS()
 class ULevelData : public UDataAsset
 {
 	GENERATED_BODY()
 
+	//@TODO Tommi - Proper data-validation
 #if WITH_EDITOR
 	virtual EDataValidationResult IsDataValid(FDataValidationContext& Context) override;
 #endif
@@ -61,4 +59,3 @@ public:
 	UPROPERTY(EditAnywhere)
 	TMap<FGameplayTag, FLevelDataStruct> LevelDatas;
 };
-
