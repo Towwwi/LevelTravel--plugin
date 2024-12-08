2dee6a86e768dbd20de37a0fb59334a3c1847c27
Towwwi <55107172+Towwwi@users.noreply.github.com>
2024-12-08 13:35:07 +0100

    Update LevelTravelFunctionLibrary.h

--- LevelTravelFunctionLibrary.h
+++ LevelTravelFunctionLibrary.h
@@ -1,4 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -6,18 +5,42 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "GameplayTagsManager.h"
 #include "GameplayTagsModule.h"
+#include "LevelData.h"
 #include "LevelTravelFunctionLibrary.generated.h"
 
-#define CHECK_GAMEPLAY_TAG_VALIDITY(GameplayTag) \
-if (!UGameplayTagsManager::Get().IsValid(GameplayTag)) \
+#define Cast_TO_WORLD(World) \
+if (!IsValid(World)) \
 { \
-UE_LOG(LogTemp, Error, TEXT("LevelTravel -plugin: Invalid GameplayTag: %s"), *GameplayTag.ToString()); \
-return false; \
-}
+return nullptr; \
+} \
+UWorld* WorldObject = Cast<UWorld>(World); \
+if (WorldObject == nullptr) \
+{ \
+return nullptr; \
+} \
+WorldObject
+
+struct FLevelDataStruct;
+class ULevelTravelManagerComponent;
 
 UCLASS()
 class ULevelTravelFunctionLibrary : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+
+public:
+	UFUNCTION(BlueprintCallable, BlueprintPure,  meta = (WorldContext = "WorldContextObject"))
+	static int32 GetCurrentPlayerCount(UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, Category="ModularLevelTravelFunctionLibrary", meta = (WorldContext = "WorldContextObject"))
+	static bool IsTravelRequestAmountEqualToPlayerCount(UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="ModularLevelTravelFunctionLibrary", meta = (WorldContext = "WorldContextObject"))
+	static ULevelTravelManagerComponent* GetLevelTravelManagerComponent(UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="ModularLevelTravelFunctionLibrary", meta = (WorldContext = "WorldContextObject"))
+	static FLevelDataStruct GetCurrentLevelData(UObject* WorldContextObject);
+
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category="ModularLevelTravelFunctionLibrary", meta = (WorldContext = "WorldContextObject"))
+	static FGameplayTag GetCurrentLevelGameplayTag(UObject* WorldContextObject);
 };
